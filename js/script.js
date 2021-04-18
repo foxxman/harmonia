1ad1b8ed0071c38fe1316b3430f2837a45293560
Максим Чупров <63455442+foxxman@users.noreply.github.com>
2021-04-19 00:18:39 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,57 +1,68 @@
 $(document).ready(function () {
+  const header = document.querySelector('.header');
+  const mainPromo = document.querySelector('.main.promo');
   const headerTop = document.querySelector(".header__top");
-  // const nets = document.querySelector(".hello__nets");
   const netsBlock = document.querySelector(".hello__nets__block");
   const list = document.querySelector(".hello__description__list");
   const netsBlockIdent =
-    (document.documentElement.clientHeight - netsBlock.clientHeight)/2;
+    (document.documentElement.clientHeight - netsBlock.clientHeight) / 2;
   //нижняя точка блока списка
   const listBottom =
     list.clientHeight + $(".hello__description__list").offset().top;
   //верхняя точка дорожки для соцсетей
   const netsTop = $(".hello__nets").offset().top;
   const maxTopAdd = listBottom - netsTop - netsBlock.clientHeight;
+
   // изменение контента в шапке при малых экранах
   if (document.documentElement.clientWidth < 860)
     headerTop.querySelectorAll("span").forEach((span, index) => {
-      const spanText = ["Покупки", "Инвестиции", "Аренда"];
+      const spanText = ["Покупка", "Инвестиции", "Аренда"];
       span.innerHTML = spanText[index];
     });
-
+  //отступ главного экрана в зависимости от высоты шапки
+  // console.log(header.clientHeight);
+  mainPromo.style=`padding-top: ${header.clientHeight}px;`
+  
   // менюшка
   $(".header__burger-menu,.menu__header__close").click(function (event) {
     // e.preventDefault();
     console.log("click");
     $(".header__burger-menu, .menu").toggleClass("active");
   });
-  console.log(document.documentElement.clientHeight);
+  // console.log(document.documentElement.clientHeight);
   //перемещение блока nets
+  // console.log(netsBlockBottom, listBottom,maxTopAdd);
   window.addEventListener("scroll", () => {
     if (document.documentElement.clientWidth > 700) {
       let netsBlockTopWindow = netsBlock.getBoundingClientRect().top; //расстояние до nets от окна
       let netsBlockTopParent = netsBlock.offsetTop; //расстояние до nets от родителя
       let netsBlockBottom =
         netsBlock.clientHeight + $(".hello__nets__block").offset().top; //нижняя точка блока с соцсетями
-      console.log();
+      // console.log();
       //расстояние от низа откна до низа блока hello__nets__block
       netsBlockBottomWindow =
         document.documentElement.clientHeight -
         netsBlockTopWindow -
         netsBlock.clientHeight;
-
+        console.log(netsBlockBottom, listBottom,maxTopAdd);
       //дв. вниз
-      if (netsBlockTopWindow <= netsBlockIdent && netsBlockBottom <= listBottom) {
+      if (
+        netsBlockTopWindow <= netsBlockIdent &&
+        netsBlockBottom <= listBottom
+      ) {
         let topAdd = netsBlockTopParent + (netsBlockIdent - netsBlockTopWindow);
         //чтобы не перескакивал через границу снизу
         if (topAdd > maxTopAdd) {
           topAdd = maxTopAdd;
         }
         netsBlock.style = `top: ${topAdd}px;`;
+        
       }
 
       //дв. вверх
       if (netsBlockBottomWindow <= netsBlockIdent && netsBlockTopParent >= 0) {
-        let topRemove = netsBlockTopParent - (netsBlockIdent - netsBlockBottomWindow);
+        let topRemove =
+          netsBlockTopParent - (netsBlockIdent - netsBlockBottomWindow);
         //чтобы не перескакивал через границу сверху
         if (topRemove < 0) {
           topRemove = 0;
