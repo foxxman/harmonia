9cf7bbc907dd889a70fd3fe48f67a17e8f82dd88
Максим Чупров <63455442+foxxman@users.noreply.github.com>
2021-05-19 01:25:58 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  console.log('load');
   const header = document.querySelector(".header");
   const main = document.querySelector(".main");
   const headerTop = document.querySelector(".header__top");
@@ -22,5 +23,69 @@ $(document).ready(function () {
   $(".header__burger-menu,.menu__header__close").click(function () {
     $(".header__burger-menu, .menu").toggleClass("active");
   });
-  
+
+  //========код для главной=====================
+  let firstScroll = true,
+    sectionChoiseTop,
+    sectionPlacesDoc,
+    topSpace,
+    bottomSpace,
+    placesBlockDoc,
+    placesBlockBottom;
+  const placesBlock = document.querySelector(".places__block");
+  const placesBlockHeight = placesBlock.getBoundingClientRect().height; //высота подвижного блока
+  const sectionChoise = document.querySelector(".choise");
+  const windowHeight = document.documentElement.getBoundingClientRect().height; //высота окна
+  const sectionPlaces = document.querySelector(".places");
+  console.log(placesBlockHeight);
+
+  //обновление переменных при скролле
+  const initVariables = () => {
+    if (firstScroll) {
+      sectionChoiseDoc = $(sectionChoise).offset().top;
+      sectionPlacesDoc = $(sectionPlaces).offset().top;
+      topSpace = (windowHeight - headerHeight - placesBlockHeight) / 2 - 1;
+      bottomSpace = topSpace;
+      firstScroll = !firstScroll;
+    }
+    placesBlockDoc = $(placesBlock).offset().top; //координата относительно документа
+    placesBlockHeader = placesBlock.getBoundingClientRect().top - headerHeight;
+    placesBlockBottom = placesBlockDoc + placesBlockHeight;
+  };
+
+  if (windowWidth >= 1240)
+    window.addEventListener("scroll", () => {
+      initVariables();
+      // console.log(placesBlockHeader, placesBlockDoc, windowHeight);
+      //начало движения когда дошли сверху
+      if (
+        placesBlockHeader < topSpace &&
+        (sectionChoiseDoc - placesBlockBottom) > bottomSpace
+      ) {
+        let top = topSpace + headerHeight;
+        placesBlock.style = `position: fixed; top: ${top}px;`;
+        console.log("down");
+        console.log((sectionChoiseDoc - placesBlockBottom) > bottomSpace )
+      }
+      // //остановка когда идем вверх
+      if (placesBlockDoc < sectionPlacesDoc) {
+        placesBlock.style = ``;
+        console.log('stop up');
+      }
+      // //остановка когда долистали до низа
+      
+      if (sectionChoiseDoc - placesBlockBottom < bottomSpace) {
+        placesBlock.style = `position: absolute; top: ${
+          $(sectionChoise).offset().top - bottomSpace - placesBlockHeight
+        }px;`;
+        console.log("stop down");
+        console.log(sectionChoiseDoc, placesBlockBottom, bottomSpace)
+      }
+      // //начало движения когда дошли снизу
+      if (placesBlockHeader > topSpace && placesBlockDoc > sectionPlacesDoc) {
+        let top = topSpace + headerHeight;
+        placesBlock.style = `position: fixed; top: ${top}px;`;
+        console.log('up');
+      }
+    });
 });
